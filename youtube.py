2c62bf24df863ef556484fea3ace489d0c7c13cc
PRATIK PATIL <68728976+prateek175@users.noreply.github.com>
2020-07-25 15:48:12 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,13 +1,28 @@
 import pytube
 import pytube.streams
-from tkinter import *
-from tkinter import ttk
+from tkinter import *   
+from tkinter import ttk   #not required line.......
 
 
-def youtube():
+def youtube():                          #define function for download the youtube video through video link(URL)...........
+    link = url.get()                     #url.get()->>> Help to retrive Url Content 
+    yt = pytube.YouTube(link)            #creating a variable ' yt ' which help to store         
+    stream = yt.streams.first()       
+    stream.download('C:/Users/Pratik Patil/Desktop/gallery') #location where you want to store.......
+    
+    f = stream.filesize                              """ .........................use show size and percentage when  video fully download.................................."""
+    MaxFileSize = int(f / 1024000)     
+    MB = str(MaxFileSize) + " MB"
+    lab_mb.set(MB)
+
+    percent = int((101 * (f - MaxFileSize)) / f)
+    per=str(percent)+"%"
+    lab_percent.set(per)
+
+def audio():                     #def foe to get Audio File ....
     link = url.get()
     yt = pytube.YouTube(link)
-    stream = yt.streams.first()
+    stream=yt.streams.get_audio_only('webm')    #get_audio_only method use for the  get the audio ... 
     stream.download('C:/Users/Pratik Patil/Desktop/gallery')
     f = stream.filesize
     MaxFileSize = int(f / 1024000)
@@ -18,23 +33,9 @@ def youtube():
     per=str(percent)+"%"
     lab_percent.set(per)
 
-def audio():
-    link = url.get()
-    yt = pytube.YouTube(link)
-    stream=yt.streams.get_audio_only('webm')
-    stream.download('C:/Users/Pratik Patil/Desktop/gallery')
-    f = stream.filesize
-    MaxFileSize = int(f / 1024000)
-    MB = str(MaxFileSize) + " MB"
-    lab_mb.set(MB)
-
-    percent = int((101 * (f - MaxFileSize)) / f)
-    per=str(percent)+"%"
-    lab_percent.set(per)
 
 
-
-Ck = Tk()
+Ck = Tk()                                 #TKINTER 
 url = StringVar()
 lab_percent = StringVar()
 lab_mb = StringVar()
